bb2820e3495b2d2b8d9964cb1dc988b9bcd4e31a
audreymendzef <audreymendzef@gmail.com>
2023-01-04 13:44:53 -0500

    feat: made ag grid editable with options

--- src/app/components/book-item/book-item.component.ts
+++ src/app/components/book-item/book-item.component.ts
@@ -2,13 +2,7 @@ import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BookService } from 'src/app/services/book.service';
 import { AgGridAngular } from 'ag-grid-angular';
-import {
-  CellClickedEvent,
-  ColDef,
-  GridReadyEvent,
-  GridOptions,
-} from 'ag-grid-community';
-
+import { ColDef, GridReadyEvent, GridOptions } from 'ag-grid-community';
 import { Book } from 'src/app/Book';
 
 @Component({
@@ -17,8 +11,6 @@ import { Book } from 'src/app/Book';
   styleUrls: ['./book-item.component.css'],
 })
 export class BookItemComponent implements OnInit {
-  @Input() book!: Book;
-
   constructor(private bookService: BookService) {
     this.gridOptions = {
       columnDefs: this.columnDefs(),
@@ -32,25 +24,90 @@ export class BookItemComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  public rowData$!: Observable<any[]>;
+  public rowData$!: Observable<Book[]>;
 
   @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
 
   public columnDefs(): ColDef[] {
     return [
-      { headerName: 'Type', field: 'type', width: 100 },
-      { headerName: 'Genre', field: 'genre', width: 170 },
-      { headerName: 'Title', field: 'title', width: 250 },
-      { headerName: 'Status', field: 'status', width: 135 },
-      { headerName: 'Score', field: 'score', width: 90 },
-      { headerName: 'Author', field: 'author', width: 175 },
+      {
+        headerName: 'Type',
+        field: 'type',
+        editable: true,
+        cellEditor: 'agSelectCellEditor',
+        width: 125,
+        cellEditorParams: {
+          values: ['Fiction', 'Non-Fiction'],
+        },
+      },
+      {
+        headerName: 'Genre',
+        field: 'genre',
+        editable: true,
+        width: 170,
+        cellEditor: 'agSelectCellEditor',
+        cellEditorParams: {
+          values: [
+            'Romance',
+            'Fantasy',
+            'Sci-Fi',
+            'War Drama',
+            'Dystopian',
+            'Dark Romance',
+            'Historical Fiction',
+            'Drama Romance',
+            'Sad Romance',
+            'Greek Myth Drama',
+            'Smut',
+            'Coming of Age Romance',
+            'Coming of Age',
+            'LGBT Romance',
+            'Rom-Com',
+            'Mystery',
+            'Drama Mystery',
+            'Action/Adventure',
+            'Thriller/Suspense',
+            'Horror',
+            'Young Adult',
+            'AutoBiography',
+            'Biography',
+            'Self-Help',
+            'History',
+            'Travel',
+            'True Crime',
+            'Essays',
+          ],
+        },
+      },
+      { headerName: 'Title', field: 'title', editable: true, width: 250 },
+      {
+        headerName: 'Status',
+        field: 'status',
+        editable: true,
+        cellEditor: 'agSelectCellEditor',
+        width: 135,
+        cellEditorParams: {
+          values: ['Want to Read', 'Reading', 'Finished'],
+        },
+      },
+      {
+        headerName: 'Score',
+        field: 'score',
+        editable: true,
+        cellEditor: 'agSelectCellEditor',
+        cellEditorParams: {
+          values: ['1', '2', '3', '4', '5'],
+        },
+        width: 90,
+      },
+      { headerName: 'Author', field: 'author', editable: true, width: 175 },
     ];
   }
   public defaultColDef: ColDef = {
     sortable: true,
     filter: true,
+    resizable: true,
   };
 
   gridOptions: GridOptions;
-  rowData: any;
 }
